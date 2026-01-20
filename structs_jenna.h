384696d246b4b1debd5a980cb5c5ca4195082635
Jenna Mustajärvi <jjen-_@hotmail.com>
2026-01-20 21:05:50 +0000

    changing struct to have last pointer to the list

--- structs_jenna.h
+++ structs_jenna.h
@@ -4,11 +4,13 @@
 typedef enum	s_token_type
 {
 	CMD,
+	BLT,
+	FILE
+	PIPE,
 	RD_IN,
 	RD_OUT,
 	APPEND,
 	HEREDOC,
-	PIPE,
 	ARG,
 } t_token_type;
 
@@ -17,14 +19,14 @@ typedef struct	s_token
 	t_token_type	type;
 	char			*str;
 	struct s_token	*next;
-	int				index;
+	struct s_token	*prev;
 } t_token;
 
 typedef struct	s_line
 {
 	t_token			*node;
 	struct s_line	*head;
-	struct s_line	*current;
+	struct s_line	*last_node;
 	struct s_line	*temp;//NOTE: allocate memory to this pointer somewhere ?
 
 } t_line;
